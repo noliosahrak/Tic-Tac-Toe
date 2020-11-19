18210e26260a850f43cfc5b0d74cdae56526aa20
noliosahrak <wsburghardt@gmail.com>
2020-11-19 16:04:10 +0100

    Let's play

--- main.py
+++ main.py
@@ -1,67 +1,73 @@
-def print_board(board):
+def print_board(field):
     print("---------")
-    print("| {} {} {} |".format(board[0], board[1], board[2]))
-    print("| {} {} {} |".format(board[3], board[4], board[5]))
-    print("| {} {} {} |".format(board[6], board[7], board[8]))
+    print("| {} {} {} |".format(field[0], field[1], field[2]))
+    print("| {} {} {} |".format(field[3], field[4], field[5]))
+    print("| {} {} {} |".format(field[6], field[7], field[8]))
     print("---------")
 
 
-def wins(board, symbol):
-    if board[4] == symbol:
-        if board[3] == symbol and board[5] == symbol:
+def wins(field, symbol):
+    if field[4] == symbol:
+        if field[3] == symbol and field[5] == symbol:
             return True
-        if board[2] == symbol and board[6] == symbol:
+        if field[2] == symbol and field[6] == symbol:
             return True
-        if board[1] == symbol and board[7] == symbol:
+        if field[1] == symbol and field[7] == symbol:
             return True
-        if board[0] == symbol and board[8] == symbol:
+        if field[0] == symbol and field[8] == symbol:
             return True
-    elif board[0] == symbol:
-        if board[1] == symbol and board[2] == symbol:
+    elif field[0] == symbol:
+        if field[1] == symbol and field[2] == symbol:
             return True
-        if board[3] == symbol and board[6] == symbol:
+        if field[3] == symbol and field[6] == symbol:
             return True
-    elif board[8] == symbol:
-        if board[2] == symbol and board[5] == symbol:
+    elif field[8] == symbol:
+        if field[2] == symbol and field[5] == symbol:
             return True
-        if board[6] == symbol and board[7] == symbol:
+        if field[6] == symbol and field[7] == symbol:
             return True
     else:
         return False
 
 
-def game_state(board):
-    if abs(board.count("X") - board.count("O")) > 1:
-        print("Impossible")
-    elif wins(board, "X"):
-        if wins(board, "O"):
-            print("Impossible")
-        else:
-            print("X wins")
-    elif wins(board, "O"):
-        print("O wins")
-    elif "_" in board:
-        print("Game not finished")
+def game_state(field):
+    if wins(field, "X"):
+        return "X wins"
+    elif wins(field, "O"):
+        return "O wins"
+    elif "_" in field:
+        return "Game not finished"
     else:
-        print("Draw")
+        return "Draw"
 
 
-board = list(input("Enter cells: "))
+def move(symbol):
+    while True:
+        coordinates = input("Enter the coordinates: ").split()
+        if coordinates[0].isdigit() and coordinates[1].isdigit():
+            coordinates = [int(x) for x in coordinates]
+            if coordinates[0] in range(1, 4) and coordinates[1] in range(1, 4):
+                address = coordinates[0] + 8 - 3 * coordinates[1]
+                global board
+                if board[address] != "_":
+                    print("This cell is occupied! Choose another one!")
+                    continue
+                board[address] = symbol
+                print_board(board)
+                break
+            else:
+                print("Coordinates should be from 1 to 3!")
+        else:
+            print("You should enter numbers!")
+
+
+board = "_ _ _ _ _ _ _ _ _".split()
 print_board(board)
-while True:
-    coordinates = input("Enter the coordinates: ").split()
-    if coordinates[0].isdigit() and coordinates[1].isdigit():
-        coordinates = [int(x) for x in coordinates]
-        if coordinates[0] in range(1, 4) and coordinates[1] in range(1, 4):
-            address = coordinates[0] + 8 - 3 * coordinates[1]
-            if board[address] != "_":
-                print("This cell is occupied! Choose another one!")
-                continue
-            board[address] = "X"
-            print_board(board)
-            # game_state(board)
-            break
-        else:
-            print("Coordinates should be from 1 to 3!")
+turn_X = True
+while game_state(board) == "Game not finished":
+    if turn_X:
+        move("X")
     else:
-        print("You should enter numbers!")
+        move("O")
+    turn_X = not turn_X
+print(game_state(board))
